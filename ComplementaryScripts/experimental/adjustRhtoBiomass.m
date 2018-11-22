8ec7223d1ae5380a1d1f84c6f220a3257c0dc321
Eduard Kerkhoven <eduardk@chalmers.se>
2018-11-22 17:08:35 +0100

    fix: curation of lipid reactions

--- ComplementaryScripts/experimental/adjustRhtoBiomass.m
+++ ComplementaryScripts/experimental/adjustRhtoBiomass.m
@@ -7,31 +7,36 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Load model
-model=importModel('../../Rhodosporidium_toruloides-GEM/ModelFiles/xml/rhto.xml');
-
+model=importModel('../../../Rhodosporidium_toruloides-GEM/ModelFiles/xml/rhto.xml');
+    
 data=readTiukovaData(1);
 
 % Change lipid backbone composition
 rxnIdx                  =   getIndexes(model,'r_4063','rxns');
 metIdx                  =   getIndexes(model,data.lipidData.metIDs,'mets');
+%metIdx                  =   getIndexes(model,strcat(data.lipidData.metNames,' backbone[c]'),'metscomps');
 bbIdx                   =   getIndexes(model,'s_3746','mets');
 model.S(:,rxnIdx)       =   0;
-model.S(metIdx,rxnIdx)  =   -data.lipidData.abundance;
+model.S(metIdx,rxnIdx)  =   -data.lipidData.abundance-data.lipidData.std;
 model.S(bbIdx,rxnIdx)   =   1;
 
 % Change lipid chain length distribution
 rxnIdx                  =   getIndexes(model,'r_4065','rxns');
-metIdx                  =   getIndexes(model,data.chainData.metIDs','mets');
+metIdx                  =   cell2mat(getIndexes(model,strcat('C',data.chainData.chain,' chain'),'metnames'));
 bbIdx                   =   getIndexes(model,'s_3747','mets');
 model.S(:,rxnIdx)       =   0;
-model.S(metIdx,rxnIdx)  =   -data.chainData.abundance;
+% Normalize lipid chain data to lipid backbone level, to get reasonable
+% estimate before final scaling
+scaling = sum([data.lipidData.abundance;data.lipidData.std]);
+scaling = scaling / sum([data.chainData.abundance;data.chainData.std]);
+scaling = (data.chainData.abundance + data.chainData.std) * scaling;
+model.S(metIdx,rxnIdx)  =   -scaling;
 model.S(bbIdx,rxnIdx)   =   1;
 
 chainExIdx  = getIndexes(model,'r_4064','rxns');
 backbExIdx  = getIndexes(model,'r_4062','rxns');
 model = setParam(model,'ub',[chainExIdx,backbExIdx],1000);
 
-model = setParam(model,'obj','r_2111',1);
-sol=solveLP(model,1);
+sol=solveLP(model,1)
 
 [model,k] = scaleAbundancesRhto(model,data,'tails');
--- ComplementaryScripts/reconstruction/r5_lipids_curation.m
+++ ComplementaryScripts/reconstruction/r5_lipids_curation.m
@@ -2,18 +2,27 @@
 % levels and is not considered to reduce the complexity of lipid metabolism
 load('../../scrap/model_r4.mat');
 % Remove reactions where one of the reactants contains 16:1 or similar in
-% the metNames
-toRemove = find(contains(model.metNames,{'16:1','palmitoleate','palmitoleoyl'}));
-[row,col] = find(model.S(toRemove,:));
-col(ismember(model.rxns(col),'r_4065')) = []; % Keep lipid pseudoreaction
-model = removeReactions(model,col,true,true,true);
+% % % the metNames
+% toRemove = find(contains(model.metNames,{'16:1','palmitoleate','palmitoleoyl'}));
+% [row,col] = find(model.S(toRemove,:));
+% col(ismember(model.rxns(col),'r_4065')) = []; % Keep lipid pseudoreaction
+% model = removeReactions(model,col,true,true,true);
+
+% % Remove all SLIME reactions, will be manually added if needed
+% model = removeReactions(model,contains(model.rxnNames,'SLIME rxn'));
 
 %% Add 18:3 synthesis and transport (degradation is missing for now)
-metsToAdd.metNames={'linolenate';'linolenate';'linolenate';'linolenoyl-CoA';'linolenoyl-CoA';'linolenoyl-CoA';'linolenoyl-CoA';'linoleoyl-CoA';'linoleoyl-CoA'};
-metsToAdd.compartments={'erm';'lp';'p';'c';'erm';'lp';'p';'lp';'mm'};
+metsToAdd.metNames={'linoleate';'linolenate';'linolenoyl-CoA'};
+metsToAdd.compartments={'c';'c';'c'};
 metsToAdd.mets=generateNewIds(model,'mets','st_',length(metsToAdd.metNames));
 %metsToAdd.metFormulas
 model=addMets(model,metsToAdd); clear metsToAdd;
+
+model=addTransport(model,'c',{'erm','lp','mm'},'linolenoyl-CoA',true,false);
+model=addTransport(model,'c',{'erm','lp','mm'},'linoleoyl-CoA',true,false);
+
+model=addTransport(model,'c',{'erm','lp','mm'},'linolenate',true,false);
+model=addTransport(model,'c',{'erm','lp','mm'},'linoleate',true,false);
 
 clear rxnsToAdd
 rxnsToAdd.equations={'H+[erm] + oxygen[erm] + NADH[erm] + linoleoyl-CoA[erm] => 2 H2O[erm] + NAD[erm] + linolenoyl-CoA[erm]';...
@@ -21,42 +30,16 @@ rxnsToAdd.equations={'H+[erm] + oxygen[erm] + NADH[erm] + linoleoyl-CoA[erm] => 
     'coenzyme A[lp] + ATP[lp] + linolenate[lp] <=> diphosphate[lp] + AMP[lp] + linolenoyl-CoA[lp]';...
     'ATP[p] + coenzyme A[p] + linolenate[p] <=> AMP[p] + diphosphate[p] + linolenoyl-CoA[p]';...
     'H2O[p] + linolenoyl-CoA[p] => coenzyme A[p] + H+[p] + linolenate[p]';...
-	'ATP[c] + H2O[c] + linolenoyl-CoA[c] => ADP[c] + H+[c] + phosphate[c] + linolenoyl-CoA[p]';...
-    'linoleoyl-CoA[c] <=> linoleoyl-CoA[erm]';...
-    'linoleoyl-CoA[c] <=> linoleoyl-CoA[lp]';...
-    'linoleoyl-CoA[c] <=> linoleoyl-CoA[mm]'};
+    'ATP[c] + H2O[c] + linolenoyl-CoA[c] => ADP[c] + H+[c] + phosphate[c] + linolenoyl-CoA[p]'};
 rxnsToAdd.rxnNames={'linoleoyl-CoA desaturase (n-C18:2CoA - n-C18:3CoA), ER membrane';...
     'fatty-acid--CoA ligase (octadecatrienoate), ER membrane';...
     'fatty-acid--CoA ligase (octadecatrienoate), lipid particle';...
     'fatty-acid--CoA ligase (octadecatrienoate), peroxisome';...
     'peroxisomal acyl-CoA thioesterase (18:3)';...
-    'fatty acyl-CoA transport via ABC system (C18:3)';...
-    'linolenoyl-CoA transport, cytoplasm-ER membrane';...
-    'linolenoyl-CoA transport, cytoplasm-lipid particle';...
-    'linolenoyl-CoA transport, cytoplasm-mitochondrial membrane'};
+    'fatty acyl-CoA transport via ABC system (C18:3)'};
 rxnsToAdd.rxns=generateNewIds(model,'rxns','t_',length(rxnsToAdd.equations));;
-model=addRxns(model,rxnsToAdd,3,'',false,false); clear rxnsToAdd
-% 
-% %% Add more SLIMEr reactions
-% metsToAdd.metNames={'monoglyceride backbone','C14:0 chain','C18:2 chain','C18:3 chain'};
-% metsToAdd.metFormulas={'C3H6O2','C14H28O2','C18H32O2','C18H30O2'};
-% metsToAdd.compartments={'c';'c';'c';'c'};
-% metsToAdd.mets=generateNewIds(model,'mets','st_',length(metsToAdd.metNames));
-% model=addMets(model,metsToAdd); clear metsToAdd;
-% 
-% rxnsToAdd.rxnNames={'monoglyceride (16:0) [cytosol] SLIME rxn',...
-%     'monoglyceride (16:1) [cytosol] SLIME rxn',...
-%     'monoglyceride (18:0) [cytosol] SLIME rxn',...
-%     'monoglyceride (18:1) [cytosol] SLIME rxn'};
-% rxnsToAdd.equations={'1-monoglyceride (16:0)[c] => 0.8574 monoglyceride backbone[c] + 0.25441 C16:0 chain[c]',...
-%     '1-monoglyceride (16:1)[c] => 0.8574 monoglyceride backbone[c] + 0.25441 C16:1 chain[c]',...
-%     '1-monoglyceride (18:0)[c] => 0.8574 monoglyceride backbone[c] + 0.25441 C18:0 chain[c]',...
-%     '1-monoglyceride (18:1)[c] => 0.8574 monoglyceride backbone[c] + 0.25441 C18:1 chain[c]'};
-% rxnsToAdd.lb=[0,0,0,0];
-% rxnsToAdd.ub=[1000,1000,1000,1000];
-% rxnsToAdd.rxns=generateNewIds(model,'rxns','t_',length(rxnsToAdd.equations));
-% model=addRxns(model,rxnsToAdd,3,'',false);
-% 
+model=addRxns(model,rxnsToAdd,3,'',true,false); clear rxnsToAdd
+
 %% Curate reactions specified in lipidTemplates.txt
 fid     = fopen ('lipidTemplates.txt');
 data    = textscan(fid,'%s %s %s %s %s %s %s %s','delimiter','\t');
@@ -66,7 +49,7 @@ chain2     = data{4};   chain3     = data{5};   chain4     = data{6};
 comps      = data{7};   grRules    = data{8};
 for i = 1:length(templNames)
     clear rxnsToAdd
-
+    
     grRule  = strtrim(split(grRules{i},','));
     comp    = strtrim(split(comps{i},','));
     ch1     = strtrim(split(chain1{i},','));
@@ -119,16 +102,91 @@ for i = 1:length(templNames)
     end
     model = addRxns(model,rxnsToAdd,3,'','st_',true);
 end
+
+% Some manual reactions
+
+%% Add SLIME metabolites
+metsToAdd.metNames={'1-monoglyceride backbone','C18:2 chain','C18:3 chain'};
+metsToAdd.metFormulas={'C3H6O2','C18H32O2','C18H30O2'};
+metsToAdd.compartments={'c';'c';'c'};
+metsToAdd.mets=generateNewIds(model,'mets','st_',length(metsToAdd.metNames));
+model=addMets(model,metsToAdd); clear metsToAdd;
+
+%% Add SLIME reactions
+cd ../experimental
+data = readTiukovaData(1);
+cd ../reconstruction
+clear rxnsToAdd
+for i=1:8%length(data.lipidData.metNames)
+    % Find compartment of component in lipid species part of lipid
+    % pseudoreaction
+    compId = model.metComps(getIndexes(model,data.lipidData.metIDs(i),'mets'));
+    rxnComp = model.comps(compId);
+    % Find lipid metabolites in that compartment
+    mets = find(startsWith(model.metNames,data.lipidData.metNames(i)));
+    mets(~(model.metComps(mets) == compId)) = [];
+    mets(contains(model.metNames(mets),'backbone')) = [];
+    
+    % Loop through each metabolite
+    for j = 1:length(mets)
+        chains      = regexp(model.metNames(mets(j)),'(\d\d:\d)','match');
+        [chains,~,n]= unique(chains{1});
+        [n,~]       = histc(n,unique(n));
+        [~,coeff]   = ismember(chains,data.chainData.chain);
+        products    = [data.lipidData.metIDs(i); data.chainData.metIDs(coeff)];
+        totalMW     = sum([data.lipidData.MW(i);data.chainData.MW(coeff).*n]);
+        coeff       = [totalMW; data.chainData.MW(coeff).*n]/1000;
+        
+        rxnsToAdd.mets          = [model.mets(mets(j)); products];
+        rxnsToAdd.stoichCoeffs  = [-1; coeff];
+        rxnsToAdd.rxnNames      = strcat(model.metNames(mets(j)), ' SLIME rxn');
+        rxnsToAdd.rxnMiriams    = struct('name',{'sbo'},'value',{'SBO:0000395'});
+        rxnsToAdd.lb            = 0;
+        rxnsToAdd.rxnConfidenceScores = 1;
+        rxnsToAdd.rxns          = generateNewIds(model,'rxns','t_',1);
+        model = addRxns(model,rxnsToAdd);
+    end
+end
+clear rxnsToAdd
+for i=5:length(data.chainData.chain)
+    % Make sure transport reactions exist
+    mets            = getIndexes(model,data.chainData.FA{i},'metnames');
+    metComps        = model.comps(model.metComps(mets));
+    metComps(ismember(metComps,{'c','p','e'})) = [];
+    model           = addTransport(model,'c',metComps,data.chainData.FA{i},true,false);
+    
+    % Add SLIME reaction from cytosolic species
+    mets            = model.mets(getIndexes(model,[data.chainData.FA{i} '[c]'],'metscomps'));
+    mets            = [mets, model.mets(getIndexes(model,'fatty acid backbone[c]','metscomps'))];
+    mets            = [mets, model.mets(getIndexes(model,['C' data.chainData.chain{i} ' chain[c]'],'metscomps'))];
+    coeffs          = data.chainData.MW(i);
+    coeffs          = [-1, (coeffs+1.008)/1000, coeffs/1000];
+    rxnsToAdd.mets  = mets;
+    rxnsToAdd.stoichCoeffs = coeffs;
+    rxnsToAdd.rxnNames     = strcat(data.chainData.FA(i), ' SLIME rxn');
+    rxnsToAdd.rxnMiriams   = struct('name',{'sbo'},'value',{'SBO:0000395'});
+    rxnsToAdd.lb           = 0;
+    rxnsToAdd.rxnConfidenceScores = 1;
+    rxnsToAdd.rxns         = generateNewIds(model,'rxns','t_',1);
+    model = addRxns(model,rxnsToAdd);
+end
+
+chainExIdx  = getIndexes(model,'r_4064','rxns');
+backbExIdx  = getIndexes(model,'r_4062','rxns');
+model = setParam(model,'ub',[chainExIdx,backbExIdx],1000);
+
+sol=solveLP(model,1)
+
 %% Reactions to find gene associations for.
 % % getModelFromHomology left some OLD_sce genes that it could not find
 % % orthologs for. Additionally, the reactions that were added by MENECO and
 % % manually added for coenzyme A are still annotated with the Sce gene (not
 % % prefixed by OLD_sce_!) Try to find the responsible R. toruloides genes.
-% 
+%
 % % All Sce genes have a Y in the name, while Rhto genes do not.
 % rxnIdx=strfind(model.grRules,'Y');
 % rxnIdx=~cellfun('isempty',rxnIdx); % Get reaction indexes
-% 
+%
 % out=cell(length(find(rxnIdx)),3);
 % out(:,1)=model.rxns(rxnIdx);
 % out(:,2)=model.rxnNames(rxnIdx);
