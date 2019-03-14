50ffda4013ad0e55a5036fdd92e3cfe587e76fe6
Eduard Kerkhoven <eduardk@chalmers.se>
2019-03-14 15:25:18 +0100

    feat: reformulate FSEOF script

--- ComplementaryScripts/analysis/fseof_target_prediction.m
+++ ComplementaryScripts/analysis/fseof_target_prediction.m
@@ -1,23 +1,27 @@
 %% This script predicts metabolic engineering targets for increased
 % production of triglycerides and linolenic acid.
-model = importModel('../ModelFiles/xml/rhto.xml');
+model = importModel('../../ModelFiles/xml/rhto.xml');
 
-% Add exchange reactions for linolenic acid and triglyceride. For the
+% Add exchange reactions for linolenate and triglyceride. For the
 % triglyceride, we specifically choose 18:0/18:1/18:0-TAG (so called SOS)
 % as target. In addition to functioning as representative of triglycerides
 % for biofuel production, this SOS-TAG is a major part of cocoa-butter.
-model = addExchangeRxns(model,'out',{'st_0107','s_3032'});
+idx = getIndexes(model, {'linolenate[c]','triglyceride (1-18:0, 2-18:1, 3-18:0)[erm]'} ,'metscomps');
+
+% Add exchange reactions for products
+model = addExchangeRxns(model, 'out', idx);
+% Keep track of ids of exchange reactions
+rxn1 = model.rxns(end);
+rxn2 = model.rxns(end-1);
 
 %% Perform FSEOF for linolenic acid and TAG on glucose
-targets = FSEOF(model, 'r_2111', 'EXC_OUT_st_0107',10,0.9,...
-    'fseof_linolenic acid_glc.tab');
-targets = FSEOF(model, 'r_2111', 'EXC_OUT_s_3032',10,0.9,...
-    'fseof_TAG_glc.tab');
+model       = setParam(model, 'eq', {'r_1714', 'r_1718'}, [-1, 0]);
+
+targets = FSEOF(model, 'r_2111', rxn1, 10, 0.9, 'fseof_linolenic acid_glc.tab');
+targets = FSEOF(model, 'r_2111', rxn2, 10, 0.9, 'fseof_TAG_glc.tab');
 
 %% Perform FSEOF for linolenic acid and TAG on xylose
-model       = setParam(model,'eq','r_1714',0);
-model       = setParam(model,'eq','r_1718',-1);
-targets = FSEOF(model, 'r_2111', 'EXC_OUT_s_3032',10,0.9,...
-    'fseof_TAG_xyl.tab');
-targets = FSEOF(model, 'r_2111', 'EXC_OUT_st_0107',10,0.9,...
-    'fseof_linolenic acid_xyl.tab');
+model       = setParam(model, 'eq', {'r_1714', 'r_1718'}, [0, -1]);
+
+targets = FSEOF(model, 'r_2111', rxn1, 10, 0.9, 'fseof_linolenic acid_xyl.tab');
+targets = FSEOF(model, 'r_2111', rxn2, 10, 0.9, 'fseof_TAG_xyl.tab');
