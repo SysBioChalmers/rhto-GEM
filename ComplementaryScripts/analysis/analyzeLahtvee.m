2259dda4253f82525eb3c2b2d6f4c6c32ff12cbe
Eduard Kerkhoven <eduardk@chalmers.se>
2019-04-02 15:14:12 +0200

    feat: models based on data from Lahtvee study

---
+++ ComplementaryScripts/analysis/analyzeLahtvee.m
@@ -0,0 +1,91 @@
+root  = regexprep(pwd(),'(.*)\\[^\\]*\\.*','$1');
+scripts = [root '/ComplementaryScripts'];
+data    = [root '/ComplementaryData'];
+cd([scripts '/experimental/']);
+load([root '/scrap/model_r8.mat']);
+
+expDat  = readLahtveeData();
+%% Generate glucose model from Tiukova data
+tiukovaData = readTiukovaData(model,2);
+model = adjustRhtoBiomass(model,tiukovaData);
+%% Readjust some reactions
+% Ensure that exchange reactions for carbon sources are reversible
+model.rev(getIndexes(model,{'r_1634','r_1718','r_1808'},'rxns')) = 1;
+% Add protein excretion reation.
+model = addExchangeRxns(model,'out','s_3717');
+lipidPos        = find(strcmp(model.rxnNames,'lipid backbone pseudoreaction'));
+scaleLp         = getIndexes(model,{'s_3733','s_1524','s_0666','s_0694'},'mets');
+
+for i=1:length(expDat.sample)
+    %% Scale lipids
+    models{i} = model;
+    %% Set exchange fluxes
+    models{i} = setGAM(models{i},0);
+    models{i} = setParam(models{i},'var',{'r_1634','r_1808','r_1718','r_1714'},...
+        [-expDat.rates(i,2:4),0],5);
+    fL = 1.01;
+    while abs(fL-1) > 0
+        fL0 = fL;
+        models{i}.S(scaleLp,lipidPos) = full(models{i}.S(scaleLp,lipidPos))*fL;
+        [~,~,~,~,~,L]   = sumBioMass(models{i});
+        fL              = expDat.lipids(i)/L;
+        disp(['Scaling lipid data: fL = ' num2str(fL0) ' -> difference = ' num2str(fL)])
+    end
+    %% Scale protein & other biomass components
+    [models{i},GAMpol] = changeOtherComp(models{i},expDat,i);
+    chainExIdx  = getIndexes(models{i},'r_4064','rxns');
+    backbExIdx  = getIndexes(models{i},'r_4062','rxns');
+    models{i} = setParam(models{i},'ub',[chainExIdx,backbExIdx],1000);
+    %sol=solveLP(models{i});
+    [models{i},k] = scaleLipidsRhto(models{i},'tails');
+    models{i} = setParam(models{i},'eq',[chainExIdx,backbExIdx],0);
+    models{i} = setParam(models{i},'lb','r_4046',0); % NGAM
+    %% Protein excretion
+    if ~(expDat.rates(i,9) == 0)
+        [~,P]  = sumBioMass(models{i}); % Weight of protein pseudometabolite
+        pIdx   = getIndexes(models{i},'EXC_OUT_s_3717','rxns');
+        models{i}.S(find(models{1}.S(:,pIdx)),pIdx) = -1/P; % Make flux represent 1 g/gDCW/h
+        models{i} = setParam(models{i},'var','EXC_OUT_s_3717',expDat.rates(i,9),5);
+    end
+    % Growth rate
+    sol(1,i)=solveLP(models{i},1);
+    disp(['Growth rate in sample ' num2str(i) ': ' num2str(-sol(1,i).f)]);
+end
+models2=models;
+models=models2;
+%% Set further constraints.
+for i=1:numel(models)
+    disp(['NGAM of condition ' expDat.sample{i} ' after fixing:']);
+    % Set NGAM as objective function and track this while setting
+    % constraints
+    models{i} = setParam(models{i},'var','r_2111',expDat.rates(i,1),5); % Growth
+    models{i} = setParam(models{i},'obj','r_4046',1);
+    sols(i)=solveLP(models{i});
+    one=num2str(-sols(i).f,4);
+    models{i} = setParam(models{i},'var','r_2104',expDat.rates(i,7),5); % Xylitol
+    sols(i)=solveLP(models{i},1);
+    two=num2str(-sols(i).f,4);
+    models{i} = setParam(models{i},'ub','r_1672',expDat.rates(i,5)); % CO2
+    sols(i)=solveLP(models{i});
+    three=num2str(-sols(i).f,4);
+    models{i} = setParam(models{i},'lb','r_1992',expDat.rates(i,6)); % O2
+    sols(i)=solveLP(models{i});
+    four=num2str(-sols(i).f,4);
+    models{i} = setParam(models{i},'ub','r_1654',-expDat.rates(i,8)); % Ammonium
+    sols(i)=solveLP(models{i},1);
+    five=num2str(-sols(i).f,4);    
+    disp('Growth  Xylitol CTR     OTR     Ammonium');
+    disp([one '   ' two '   ' three '   ' four '   ' five]);
+end
+
+idx = getIndexes(models{1},{'r_2111','r_1634','r_1808','r_1718','r_1672','r_1992','r_2104','r_1654','EXC_OUT_s_3717','r_4046'},'rxns')
+
+clear out;for i=1:numel(models); out(i,:) = sols(i).x(idx); end; out=transpose(out);
+
+[~,idx] = getExchangeRxns(model,'both');
+clear out;for i=1:numel(models); out(i,:) = sols(i).x(idx); end
+rmIdx = sum(out,1) == 0;
+idx = idx(~rmIdx);
+out = out(:,~rmIdx);
+model.rxnNames(idx)
+[mets, ~] = find(model.S(:,idx))
---
+++ ComplementaryScripts/experimental/readLahtveeData.m
@@ -0,0 +1,12 @@
+function data = readLahtveeData()
+fid = fopen('../../ComplementaryData/data/fluxData_Lahtvee2019.csv');
+types = textscan(fid,repmat('%s',[1,12]),1,'Delimiter',',');
+dat = textscan(fid,['%s' repmat(' %f32 ',[1,11])],'Delimiter',',','HeaderLines',1);
+fclose(fid);
+data.types      = horzcat(types{2:end});
+data.sample     = dat{1};
+data.rates      = cat(2,dat{[2:10]});
+data.rates(isnan(data.rates)) = 0;
+data.lipids     = dat{11};
+data.protein    = dat{12};
+end
